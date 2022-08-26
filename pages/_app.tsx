13a9a899fe747e5303ebbd6e5fc2ae5f9243d549
Devinda Senanayake <3579142+dca123@users.noreply.github.com>
2022-08-26 21:06:01 +0530

    feat: disable SSR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,13 +15,12 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
 export default withTRPC<AppRouter>({
   config({ ctx }) {
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
+    const url = process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
       : "http://localhost:3000/api/trpc";
     return {
       transformer: superjson,
       url,
     };
   },
-  ssr: true,
 })(MyApp);
