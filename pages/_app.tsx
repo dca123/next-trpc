48138b56057a7bf3423270711401cd7fc0308aa6
Devinda Senanayake <3579142+dca123@users.noreply.github.com>
2022-08-26 21:22:00 +0530

    feat: :busts_in_silhouette: use tanstack query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,3 @@
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { withTRPC } from "@trpc/next";
 import { AppRouter } from "./api/trpc/[trpc]";
@@ -15,12 +14,24 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
 
 export default withTRPC<AppRouter>({
   config({ ctx }) {
-    const url = process.env.NEXT_PUBLIC_VERCEL_URL
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
       : "http://localhost:3000/api/trpc";
     return {
-      transformer: superjson,
       url,
+      transformer: superjson,
+      /**
+       * @link https://react-query.tanstack.com/reference/QueryClient
+       */
+      // queryClientConfig: { defaultOptions: { queries: { staleTime: 60 } } },
     };
   },
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  ssr: true,
 })(MyApp);
